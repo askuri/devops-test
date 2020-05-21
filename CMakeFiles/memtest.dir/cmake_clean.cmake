989f82070db43483d29c4e230d2c0c72ead611ef
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-21 18:27:17 +0000

    Deploy askuri/devops-test to github.com/askuri/devops-test.git:gh-pages

---
+++ CMakeFiles/memtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memtest.dir/lib.cpp.o"
+  "memtest.pdb"
+  "memtest.js"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/memtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
